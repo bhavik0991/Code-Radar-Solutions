edad8a33745cd6ac8e8b8f9fc47926941fe7de9e
bhavik0991 <bhavik0991.becse24@chitkara.edu.in>
2025-04-11 00:12:54 +0530

    Solution to Print an Alphabet Pyramid

---
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    char ch;
+    scanf("%d ",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=65;j<65+i;j++){
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+       return 0;
+}
